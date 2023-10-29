c55f452334b332443a3d1e2d1fd9a205d4e67978
RianYuri <rianyurirgt@gmail.com>
2023-10-29 08:47:42 -0300

    feat: add Analise page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Analise } from './pages/Analise';
 import { Home } from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path='/Analise' element={<Analise/>}/>
       </Routes>
     </Router>
   )
