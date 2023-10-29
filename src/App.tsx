10f40cbea62d28f12b5d4e5a7df0ea4df1bb12b2
RianYuri <rianyurirgt@gmail.com>
2023-10-28 21:40:03 -0300

    feat: add routes in a project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-
+import { Home } from './pages/home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
 
   return (
-    <>
-      App.tsx
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </Router>
   )
 }
 
